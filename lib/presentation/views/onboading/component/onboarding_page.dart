1344f7c103a090f1e3e824542a1aef2edf146c64
trongnguyenv <trongdk586@gmail.com>
2022-07-05 22:33:54 +0700

    implementation onboarding screen

---
+++ lib/presentation/views/onboading/component/onboarding_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../resources/sizes.dart';
+
+class OnBoardingPage extends StatelessWidget {
+  SliderObject _sliderObject;
+
+  OnBoardingPage(this._sliderObject, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        SizedBox(height: AppSize.s40),
+        Padding(
+          padding: const EdgeInsets.all(AppPadding.p8),
+          child: Text(
+            _sliderObject.title,
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.headline1,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(AppPadding.p8),
+          child: Text(
+            _sliderObject.subTitle,
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.subtitle1,
+          ),
+        ),
+        SizedBox(height: AppSize.s60),
+        SvgPicture.asset(_sliderObject.image),
+      ],
+    );
+  }
+}
+
+class SliderObject {
+  String title;
+  String subTitle;
+  String image;
+
+  SliderObject(this.title, this.subTitle, this.image);
+}
--- lib/presentation/views/onboading/onboarding.dart
+++ lib/presentation/views/onboading/onboarding.dart
@@ -1,5 +1,12 @@
+import 'package:complete_advanced_flutter/presentation/resources/assets.dart';
 import 'package:complete_advanced_flutter/presentation/resources/colors.dart';
+import 'package:complete_advanced_flutter/presentation/resources/sizes.dart';
+import 'package:complete_advanced_flutter/presentation/resources/strings.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'component/onboarding_page.dart';
 
 class OnBoardingView extends StatefulWidget {
   const OnBoardingView({Key? key}) : super(key: key);
@@ -9,11 +16,114 @@ class OnBoardingView extends StatefulWidget {
 }
 
 class _OnBoardingViewState extends State<OnBoardingView> {
+  late final List<SliderObject> _list = _getSliderData();
+  PageController _pageController = PageController(initialPage: 0);
+  int _currentIndex = 0;
+
+  List<SliderObject> _getSliderData() => [
+        SliderObject(AppStrings.onBoardingSubTitle1,
+            AppStrings.onBoardingSubTitle1, ImageAssets.onboardingLogo1),
+        SliderObject(AppStrings.onBoardingSubTitle2,
+            AppStrings.onBoardingSubTitle2, ImageAssets.onboardingLogo2),
+        SliderObject(AppStrings.onBoardingSubTitle3,
+            AppStrings.onBoardingSubTitle3, ImageAssets.onboardingLogo3),
+        SliderObject(AppStrings.onBoardingSubTitle4,
+            AppStrings.onBoardingSubTitle4, ImageAssets.onboardingLogo4),
+      ];
+
   @override
   Widget build(BuildContext context) {
+    Widget _getProperCircle(int index) {
+      if (index == _currentIndex) {
+        return SvgPicture.asset(ImageAssets.hollowCircleIc); // selected slider
+      } else {
+        return SvgPicture.asset(ImageAssets.solidCircleIc); // unselected slider
+      }
+    }
+
+    Widget _getBottomSheetWidget() {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // left arrow
+          Padding(
+            padding: EdgeInsets.all(AppPadding.p14),
+            child: GestureDetector(
+              child: SizedBox(
+                height: AppSize.s20,
+                width: AppSize.s20,
+                child: SvgPicture.asset(ImageAssets.leftArrowIc),
+              ),
+              onTap: () {
+                // go to next slide
+              },
+            ),
+          ),
+          // circles indicator
+          Row(
+            children: [
+              for (int i = 0; i < _list.length; i++)
+                Padding(
+                  padding: EdgeInsets.all(AppPadding.p8),
+                  child: _getProperCircle(i),
+                )
+            ],
+          ),
+          // right arrow
+          Padding(
+            padding: EdgeInsets.all(AppPadding.p14),
+            child: GestureDetector(
+              child: SizedBox(
+                height: AppSize.s20,
+                width: AppSize.s20,
+                child: SvgPicture.asset(ImageAssets.rightarrowIc),
+              ),
+              onTap: () {
+                // go to next slide
+              },
+            ),
+          )
+        ],
+      );
+    }
+
     return Scaffold(
       backgroundColor: AppColors.white,
-      body: Center(child: Text("Welcome to on boarding")),
+      appBar: AppBar(
+        elevation: AppSize.s1_5,
+        systemOverlayStyle: SystemUiOverlayStyle(
+            statusBarColor: AppColors.white,
+            statusBarBrightness: Brightness.dark,
+            statusBarIconBrightness: Brightness.dark),
+      ),
+      body: PageView.builder(
+        controller: _pageController,
+        itemCount: _list.length,
+        onPageChanged: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        itemBuilder: (context, index) {
+          return OnBoardingPage(_list[index]);
+        },
+      ),
+      bottomSheet: Container(
+        color: AppColors.white,
+        height: AppSize.s100,
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                  onPressed: () {},
+                  child: Text(AppStrings.skip, textAlign: TextAlign.end)),
+            ),
+            // add layout for indicator and arrows
+            _getBottomSheetWidget(),
+          ],
+        ),
+      ),
     );
   }
 }
